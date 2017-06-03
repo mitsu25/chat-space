bdb700d21da59bfd96bd3dcecb8ef30595bd1265
mitsu25 <m.nakano65@gmail.com>
2017-06-03 17:33:20 +0900

    set actions for records

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,6 +5,29 @@ class GroupsController < ApplicationController
   end
 
   def create
-    binding.pry
+    @group = Group.create(name:group_params[:name])
+    create_records_on_user_group_table(@group)
+    redirect_to messages_path
   end
+
+  private
+  # paramsからgroupnameとuser_group_idsを取得するメソッド
+  def group_params
+    params.require(:group).permit(:name,user_group_ids:[])
+  end
+
+  # user_group_idsの先頭の空要素を取り出して新配列を返すメソッド
+  def get_user_ids
+    user_ids = group_params[:user_group_ids]
+    user_ids.shift
+    return user_ids
+  end
+
+  # idsとGroupインスタンスのidで中間テーブルにレコードを残すメソッド
+  def create_records_on_user_group_table(group)
+    get_user_ids.each do |id|
+      UserGroup.create(user_id:id,group_id:group.id)
+    end
+  end
+
 end
