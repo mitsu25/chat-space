2521ba706c4e5b78eebe863d5d795dfd3a473fdb
mitsu25 <m.nakano65@gmail.com>
2017-06-03 11:48:44 +0900

    generate user_group model and table

---
+++ db/migrate/20170603024533_create_user_groups.rb
@@ -0,0 +1,9 @@
+class CreateUserGroups < ActiveRecord::Migration[5.0]
+  def change
+    create_table :user_groups do |t|
+      t.references  :user_id
+      t.references  :group_id
+      t.timestamps
+    end
+  end
+end
