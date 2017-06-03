299e55043de46fbf618360e93f6d7576643b9816
mitsu25 <m.nakano65@gmail.com>
2017-06-03 12:48:50 +0900

    set associations to each tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :user_groups
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
