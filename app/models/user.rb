b00aeebe02f1dce87e55626ba660fba4605dccac
mitsu25 <m.nakano65@gmail.com>
2017-06-03 14:57:24 +0900

    set input form in new group page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :user_groups
+  has_many :groups, through: :user_groups
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
