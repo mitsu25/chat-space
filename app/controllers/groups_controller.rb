daa852568b1cb0484e5e5ba720292fdfa7e44f7a
mitsu25 <m.nakano65@gmail.com>
2017-06-05 13:54:25 +0900

    revise create action for group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,8 +5,8 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group = Group.create(name:group_params[:name])
-    create_records_on_user_group_table(@group)
+    @group = Group.create(name:group_params[:name],user_ids:user_ids)
+    # create_records_on_user_group_table(@group)
     render action: :new
   end
 
@@ -17,17 +17,10 @@ class GroupsController < ApplicationController
   end
 
   # user_group_idsの先頭の空要素を取り出して新配列を返すメソッド
-  def get_user_ids
+  def user_ids
     user_ids = group_params[:user_group_ids]
     user_ids.shift
     return user_ids
-  end
-
-  # idsとGroupインスタンスのidで中間テーブルにレコードを残すメソッド
-  def create_records_on_user_group_table(group)
-    get_user_ids.each do |id|
-      UserGroup.create(user_id:id,group_id:group.id)
-    end
   end
 
 end
