9b64b5827569bfebf6d9233eaeae59aac7447cb4
mitsu25 <m.nakano65@gmail.com>
2017-06-05 18:40:00 +0900

    revise controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,45 +1,38 @@
 class GroupsController < ApplicationController
+  before_action :get_group, only: [:edit, :update]
 
   def new
     @group = Group.new
   end
 
   def create
-    @group = Group.new(name: group_params[:name],user_ids: user_ids)
+    @group = Group.new(name: group_params[:name],user_ids: group_params[:user_group_ids])
     if @group.save
-      flash.now[:notice] = "新規グループが作成されました"
-      render template: "messages/index"
+      flash.keep[:notice] = "新規グループが作成されました"
+      redirect_to messages_path
     else
-      render action: :new
+      render :new
     end
   end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
   def update
-    @group = Group.find(params[:id])
-    if @group.update(name: group_params[:name],user_ids: user_ids)
-      flash.now[:notice] = "チャットグループが更新されました"
-      @groups = Group.all
-      render template: "messages/index"
+    if @group.update(group_params)
+      flash.keep[:notice] = "チャットグループが更新されました"
+      redirect_to messages_path
     else
-      render action: :edit
+      render :edit
     end
   end
 
   private
+  def get_group
+    @group = Group.find(params[:id])
+  end
   # paramsからgroupnameとuser_group_idsを取得するメソッド
   def group_params
-    params.require(:group).permit(:name,user_group_ids:[])
+    params.require(:group).permit(:name, {user_ids: []})
   end
-
-  # user_group_idsの先頭の空要素を取り出して新配列を返すメソッド
-  def user_ids
-    user_ids = group_params[:user_group_ids]
-    user_ids.shift
-    return user_ids
-  end
-
 end
