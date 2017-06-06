e4291ffe2fe2652f1e665af642ccc623557f0ca5
mitsu25 <m.nakano65@gmail.com>
2017-06-06 18:16:31 +0900

    revise validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,12 +5,6 @@ class Message < ApplicationRecord
   mount_uploader :image, ImageUploader
 
 #------validation--------------------------
-  validate :message_validation
+validates :body, presence: true, unless: :image?
 
-  private
-  def message_validation
-    if ( validates_absence_of :body ) && ( validates_absence_of :image )
-      errors.add(:message, "テキスト入力または画像添付が必要です")
-    end
-  end
 end
