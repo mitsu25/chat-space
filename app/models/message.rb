1b9b2752f4e27dfb980457fbc0dc877cde8090be
mitsu25 <m.nakano65@gmail.com>
2017-06-06 17:21:29 +0900

    add validation to message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,16 @@
 class Message < ApplicationRecord
+#-------Association------------------------
   belongs_to :user
   belongs_to :message
   mount_uploader :image, ImageUploader
+
+#------validation--------------------------
+
+  validate :message_validation
+
+  def message_validation
+    if ( validates_absence_of :body ) && ( validates_absence_of :image )
+      errors.add(:message, "Either text or image must to be filled")
+    end
+  end
 end
