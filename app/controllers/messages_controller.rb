27229b5fa80c562d799bee850d13ce1b8643411f
mitsu25 <m.nakano65@gmail.com>
2017-06-06 16:57:06 +0900

    modify controller to show message#index view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,15 +3,16 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @group  = Group.find(params[:group_id])
     @groups = Group.joins(:users).where(users: {id:current_user.id}).includes(:users, :messages)
     @message = Message.new
   end
 
   def create
     @message = Message.new(message_params)
-    if @group.save
+    if @message.save
       flash.keep[:notice] = "メッセージを投稿しました。"
-      redirect_to group_path(params[:group_id])
+      redirect_to group_messages_path(params[:group_id])
     else
       render controller: :groups, action: :show
     end
