7bc2e9fec9a0c3e31c69ceb34e27adff84083a8e
mitsu25 <m.nakano65@gmail.com>
2017-06-06 18:17:22 +0900

    refactoring in message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,9 @@
 class MessagesController < ApplicationController
 
   before_action :authenticate_user!
+  before_action :get_group, only: [:index, :create]
 
   def index
-    @group  = Group.find(params[:group_id])
     @groups = Group.joins(:users).where(users: {id:current_user.id}).includes(:users, :messages)
     @message = Message.new
   end
@@ -12,15 +12,19 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     if @message.save
       flash.keep[:notice] = "メッセージを投稿しました。"
-      redirect_to group_messages_path(params[:group_id])
+      redirect_to group_messages_path(@group)
     else
       flash.keep[:alert] = "テキストを入力するか画像を添付してください。"
-      redirect_to group_messages_path(params[:group_id])
+      redirect_to group_messages_path(@group)
     end
   end
 
   private
   def message_params
     params.require(:message).permit(:body, :image).merge(user_id:current_user.id,group_id:params[:group_id])
+  end
+
+  def get_group
+    @group  = Group.find(params[:group_id])
   end
 end
