731453bd6c19e6ec98aba249c741c16e6540fb0c
mitsu25 <m.nakano65@gmail.com>
2017-06-08 13:46:01 +0900

    add spec for situation without user_id, group_id

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -29,6 +29,18 @@ describe Message do
         expect(message.errors[:body]).to include("を入力してください")
       end
 
+      it "is invalid without a user_id" do
+        message = build(:message, user_id:"")
+        message.valid?
+        expect(message.errors[:user_id]).to include("を入力してください")
+      end
+
+      it "is invalid without a group_id" do
+        message = build(:message, group_id:"")
+        message.valid?
+        expect(message.errors[:group_id]).to include("を入力してください")
+      end
+
     end
 
   end
