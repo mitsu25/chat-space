8fee73f2af42cd6d67e440a9d127d5e5f7138a2f
mitsu25 <m.nakano65@gmail.com>
2017-06-08 13:45:25 +0900

    add validation for user_id, group_id in message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,6 +5,8 @@ class Message < ApplicationRecord
   mount_uploader :image, ImageUploader
 
 #------validation--------------------------
-validates :body, presence: true, unless: :image?
+validates :body    , presence: true, unless: :image?
+validates :user_id , presence: true
+validates :group_id, presence: true
 
 end
