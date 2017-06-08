7061dbb7d3ce8f84b204574c82e4f85a08c596c9
mitsu25 <m.nakano65@gmail.com>
2017-06-08 12:21:58 +0900

    grouping examples with contexts in message spec

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,25 +2,33 @@ require 'rails_helper'
 describe User do
   describe '#create' do
 
-    it "is valid with a body, a image, a user_id, a group_id" do
-      message = build(:message)
-      expect(message).to be_valid
+    context "it is valid" do
+
+      it "is valid with a body, a image, a user_id, a group_id" do
+        message = build(:message)
+        expect(message).to be_valid
+      end
+
+      it "is valid with body and without image" do
+        message = build(:message, image:"")
+        expect(message).to be_valid
+      end
+
+      it "is valid with image and without body" do
+        message = build(:message, body:"")
+        expect(message).to be_valid
+      end
+
     end
 
-    it "is valid with body and without image" do
-      message = build(:message, image:"")
-      expect(message).to be_valid
-    end
+    context "it is invalid" do
 
-    it "is valid with image and without body" do
-      message = build(:message, body:"")
-      expect(message).to be_valid
-    end
+      it "is invalid withou neither body nor image" do
+        message = build(:message, body:"", image:"")
+        message.valid?
+        expect(message.errors[:body]).to include("を入力してください")
+      end
 
-    it "is invalid withou neither body nor image" do
-      message = build(:message, body:"", image:"")
-      message.valid?
-      expect(message.errors[:body]).to include("を入力してください")
     end
 
   end
