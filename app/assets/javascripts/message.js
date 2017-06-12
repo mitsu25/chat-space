462e9f0e884254377f32389f7ec7b6f93ba5cdc4
mitsu25 <m.nakano65@gmail.com>
2017-06-12 14:03:13 +0900

    modify appearance in message js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,33 +1,40 @@
 $(function() {
 
   function buildHTML(data) {
-    var itemName = '<li class="main__chat_area__chat_content__name">' + data.userName + '</li>';
-    var itemTime = '<li class="main__chat_area__chat_content__date">' + data.createdAt + '</li>';
-    var itemBody = '<li class="main__chat_area__chat_content__text">' + data.body + '</li>';
+
     if (data.image.url === null) {
+      var itemName    = '<li class="main__chat_area__chat_content__name">' + data.userName + '</li>';
+      var itemTime    = '<li class="main__chat_area__chat_content__date">' + data.createdAt + '</li>';
+      var itemBody    = '<li class="main__chat_area__chat_content__text">' + data.body + '</li>';
       var messageList = '<ul class="main__chat_area__chat_cotentt">' + itemName + itemTime + itemBody + '</ul>';
+      return messageList;
+
     } else {
-      var image     = '<img src="' + data.image.url + '">'
-      var itemImage = '<li class="main__chat_area__chat_content__image">' + image +'</li>';
+      var itemName    = '<li class="main__chat_area__chat_content__name">' + data.userName + '</li>';
+      var itemTime    = '<li class="main__chat_area__chat_content__date">' + data.createdAt + '</li>';
+      var itemBody    = '<li class="main__chat_area__chat_content__text">' + data.body + '</li>';
+      var image       = '<img src="' + data.image.url + '">'
+      var itemImage   = '<li class="main__chat_area__chat_content__image">' + image +'</li>';
       var messageList = '<ul class="main__chat_area__chat_cotentt">' + itemName + itemTime + itemBody + itemImage + '</ul>';
+      return messageList;
     }
-    return messageList;
+
   }
 
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
-    var form = $(this).get(0);
+    var form     = $(this).get(0);
     var formData = new FormData(form);
-    var groupId = $('.group_id').attr('value');
-    var url = '/groups/' + groupId + '/messages';
+    var groupId  = $('.group_id').attr('value');
+    var url      = '/groups/' + groupId + '/messages';
 
     $.ajax({
-      type: 'POST',
-      url: url,
-      dataType: 'json',
-      data: formData,
-      processData: false,
-      contentType: false,
+      type        : 'POST',
+      url         : url,
+      dataType    : 'json',
+      data        : formData,
+      processData : false,
+      contentType : false,
     })
     .done(function(data){
       e.preventDefault();
