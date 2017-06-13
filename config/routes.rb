ed3ad32bbf870f7c1a87a2588ebb3e33f5616e65
mitsu25 <m.nakano65@gmail.com>
2017-06-13 11:20:32 +0900

    add routing for user index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root       "messages#index"
   devise_for :users
-  resources  :users,    only: [:edit,:update]
+  resources  :users,    only: [:edit,:update,:index]
   resources  :groups, except: [:index, :destroy] do
     resources  :messages, only: [:index, :create]
   end
