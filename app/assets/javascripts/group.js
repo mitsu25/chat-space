39d6da90c08ed2c3a59ddb0f065bd616000437f8
mitsu25 <m.nakano65@gmail.com>
2017-06-13 16:57:58 +0900

    add code for user select function from list

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -7,6 +7,14 @@ $(function(){
     return html;
   }
 
+  function buildHtmlForSelectedUser(userId,userName){
+    var idTag = `<input name = "group[user_ids][]", type ="hidden", value = ${ userId } >`;
+    var userNameTag = `<p class="chat-group-user__name"> ${ userName } </p>`;
+    var removeButton = '<a class="user-search-add chat-group-user__btn chat-group-user__btn--remove" >削除</a>'
+    var html = '<div class="chat-group-user clearfix">' + idTag + userNameTag + removeButton + '</div>';
+    return html;
+  }
+
   var preKeyWord;
 
   $('#user-search-field').on('keyup', function(e){
@@ -35,6 +43,21 @@ $(function(){
               var html = buildHtmlForUserSearch(userData);
               searchResultList.append(html);
             });
+
+            $('.chat-group-user__btn--add').on('click', function(){
+              var userId = $(this).attr('data-user-id');
+              var userName = $(this).attr('data-user-name');
+              var html = buildHtmlForSelectedUser(userId,userName);
+              $('#chat-group-users').append(html);
+              $('.chat-group-user__btn--remove').on('click', function(){
+                console.log("remove button was pushed");
+                $(this).parent().remove();
+              });
+              $(this).parent().remove();
+            });
+
+
+
           })
           .fail(function(){
             alert('ユーザー検索に失敗しました');
