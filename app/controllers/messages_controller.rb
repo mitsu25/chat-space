703c983867244540337dabc884f923d144cf6241
mitsu25 <m.nakano65@gmail.com>
2017-06-14 13:17:56 +0900

    set reload action in messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,14 @@
 class MessagesController < ApplicationController
 
-  before_action :get_group, only:  [:index, :create]
+  before_action :get_group, only:  [:index, :create, :reload]
   before_action :get_groups, only: [:index, :create]
 
   def index
-    @message = Message.new
+    @message  = Message.new
   end
 
   def create
-    @message = Message.new(message_params)
+    @message  = Message.new(message_params)
     if @message.save
       respond_to do |format|
         format.html { redirect_to group_messages_path(@group) }
@@ -18,6 +18,13 @@ class MessagesController < ApplicationController
     else
       flash.keep[:alert] = "テキストを入力するか画像を添付してください。"
       render :index
+    end
+  end
+
+  def reload
+    @messages = @group.messages.includes(:user)
+    respond_to do |format|
+      format.json
     end
   end
 
